164cbcd5cde3e58cb590298c8d2ce6a36c6af0ec
BAHATI Justin <bajustone@gmail.com>
2025-10-22 10:38:25 +0200

    fix

--- index.php
+++ index.php
@@ -39,22 +39,109 @@ $PAGE->set_pagelayout('standard');
 $PAGE->set_title(get_string('librarypage_title', 'local_reblibrary'));
 $PAGE->set_heading(get_string('librarypage_heading', 'local_reblibrary'));
 
+// Load custom CSS.
+$PAGE->requires->css('/local/reblibrary/styles.css');
+
 // Add breadcrumb navigation.
 $PAGE->navbar->add(get_string('pluginname', 'local_reblibrary'));
 
-// Load JavaScript module.
-$PAGE->requires->js_call_amd('local_reblibrary/library', 'init');
+/**
+ * Helper function to generate star rating array.
+ *
+ * @param int $rating Rating from 1-5
+ * @return array Array of star objects
+ */
+function generate_stars($rating) {
+    $stars = [];
+    for ($i = 1; $i <= 5; $i++) {
+        $stars[] = ['empty' => $i > $rating];
+    }
+    return $stars;
+}
+
+// Prepare header data.
+$headercontext = [
+    'logo_text' => get_string('header_logo', 'local_reblibrary'),
+    'search_placeholder' => get_string('header_search_placeholder', 'local_reblibrary'),
+    'subscribe_text' => get_string('header_subscribe', 'local_reblibrary'),
+    'user_fullname' => fullname($USER),
+    'user_picture_url' => $OUTPUT->get_generated_image_for_id($USER->id),
+    'user_initials' => strtoupper(substr($USER->firstname, 0, 1) . substr($USER->lastname, 0, 1)),
+    'has_notifications' => false,
+    'notification_count' => 0,
+];
+
+// Prepare sidebar navigation data.
+$sidebarcontext = [
+    'browse_heading' => get_string('sidebar_browse', 'local_reblibrary'),
+    'browse_items' => [
+        ['name' => get_string('nav_topbooks', 'local_reblibrary'), 'url' => '#', 'icon' => 'fa fa-star', 'active' => false],
+        ['name' => get_string('nav_discover', 'local_reblibrary'), 'url' => '#', 'icon' => 'fa fa-compass', 'active' => false],
+        ['name' => get_string('nav_categories', 'local_reblibrary'), 'url' => '#', 'icon' => 'fa fa-th-large', 'active' => false],
+    ],
+    'your_books_heading' => get_string('sidebar_your_books', 'local_reblibrary'),
+    'your_books_items' => [
+        ['name' => get_string('nav_reading', 'local_reblibrary'), 'url' => '#', 'icon' => 'fa fa-book-open', 'active' => false],
+        ['name' => get_string('nav_favorite', 'local_reblibrary'), 'url' => '#', 'icon' => 'fa fa-heart', 'active' => false],
+        ['name' => get_string('nav_history', 'local_reblibrary'), 'url' => '#', 'icon' => 'fa fa-history', 'active' => false],
+    ],
+    'shelves_heading' => get_string('sidebar_shelves', 'local_reblibrary'),
+    'shelves_items' => [
+        ['name' => get_string('nav_your_shelves', 'local_reblibrary'), 'url' => '#', 'icon' => 'fa fa-bookmark', 'active' => false],
+    ],
+    'create_shelf_text' => get_string('btn_create_shelf', 'local_reblibrary'),
+];
+
+// Placeholder book data - Recently Added.
+$recentlyadded = [
+    ['title' => 'Steve Jobs', 'author' => 'Walter Isaacson', 'cover_url' => '', 'stars' => generate_stars(3), 'has_actions' => false],
+    ['title' => 'Radical', 'author' => 'David Platt', 'cover_url' => '', 'stars' => generate_stars(3), 'has_actions' => false],
+    ['title' => 'Ender\'s Game', 'author' => 'Orson Scott Card', 'cover_url' => '', 'stars' => generate_stars(4), 'has_actions' => true],
+    ['title' => 'The Perks of Being a Wallflower', 'author' => 'Stephen Chbosky', 'cover_url' => '', 'stars' => generate_stars(0), 'has_actions' => false],
+    ['title' => 'The Hobbit', 'author' => 'J.R.R. Tolkien', 'cover_url' => '', 'stars' => generate_stars(5), 'has_actions' => false],
+    ['title' => 'Holbein - Masterpieces', 'author' => 'L.R. Henssman', 'cover_url' => '', 'stars' => generate_stars(0), 'has_actions' => false],
+    ['title' => 'The Coral Island', 'author' => 'R.M. Ballantyne', 'cover_url' => '', 'stars' => generate_stars(3), 'has_actions' => false],
+];
+
+// Placeholder book data - Recommended For You.
+$recommended = [
+    ['title' => 'An American Life', 'author' => 'Ronald Reagan', 'cover_url' => '', 'stars' => generate_stars(2), 'has_actions' => false],
+    ['title' => 'The Return of Sherlock Holmes', 'author' => 'Arthur Conan Doyle', 'cover_url' => '', 'stars' => generate_stars(3), 'has_actions' => false],
+    ['title' => 'Ender\'s Game', 'author' => 'Orson Scott Card', 'cover_url' => '', 'stars' => generate_stars(4), 'has_actions' => false],
+    ['title' => 'The Sound of Things Falling', 'author' => 'Juan Gabriel Vásquez', 'cover_url' => '', 'stars' => generate_stars(2), 'has_actions' => false],
+    ['title' => 'The Fault In Our Stars', 'author' => 'John Green', 'cover_url' => '', 'stars' => generate_stars(3), 'has_actions' => false],
+    ['title' => 'Just My Type', 'author' => 'Simon Garfield', 'cover_url' => '', 'stars' => generate_stars(5), 'has_actions' => false],
+    ['title' => 'Wake', 'author' => 'Amanda Hocking', 'cover_url' => '', 'stars' => generate_stars(3), 'has_actions' => false],
+    ['title' => 'Fearless Captain', 'author' => 'Aleck Loker', 'cover_url' => '', 'stars' => generate_stars(2), 'has_actions' => false],
+    ['title' => 'Execute', 'author' => 'Drew Wilson & Josh Long', 'cover_url' => '', 'stars' => generate_stars(3), 'has_actions' => false],
+    ['title' => 'Harry Potter and the Deathly Hallows', 'author' => 'J.K. Rowling', 'cover_url' => '', 'stars' => generate_stars(4), 'has_actions' => false],
+    ['title' => 'I Kissed Dating Goodbye', 'author' => 'Joshua Harris', 'cover_url' => '', 'stars' => generate_stars(2), 'has_actions' => false],
+    ['title' => 'White Fang', 'author' => 'Jack London', 'cover_url' => '', 'stars' => generate_stars(3), 'has_actions' => false],
+    ['title' => 'The 7 Habits of Highly Effective People', 'author' => 'Stephen R. Covey', 'cover_url' => '', 'stars' => generate_stars(4), 'has_actions' => false],
+    ['title' => 'The Harbinger', 'author' => 'Jonathan Cahn', 'cover_url' => '', 'stars' => generate_stars(5), 'has_actions' => false],
+];
+
+// Placeholder friend data.
+$friends = [
+    ['name' => 'Neil Patrick Harris', 'reading' => 'Fantasy Life - Match...', 'avatar_url' => ''],
+    ['name' => 'Robert Downey, Jr.', 'reading' => 'It\'s Just a Good Da...', 'avatar_url' => ''],
+    ['name' => 'Russell Crowe', 'reading' => 'This Town - Mark Lel...', 'avatar_url' => ''],
+];
 
 // Prepare data for template.
 $templatecontext = [
-    'welcome_message' => get_string('librarypage_welcome', 'local_reblibrary'),
-    'description' => get_string('librarypage_description', 'local_reblibrary'),
-    'placeholder_title' => get_string('librarypage_placeholder_title', 'local_reblibrary'),
-    'placeholder_text' => get_string('librarypage_placeholder_text', 'local_reblibrary'),
+    'header' => $headercontext,
+    'sidebar' => $sidebarcontext,
+    'recently_added_title' => get_string('section_recently_added', 'local_reblibrary'),
+    'recently_added' => $recentlyadded,
+    'recommended_title' => get_string('section_recommended', 'local_reblibrary'),
+    'recommended' => $recommended,
+    'friends_heading' => get_string('friends_heading', 'local_reblibrary'),
+    'friends' => $friends,
 ];
 
 // Output the page.
 echo $OUTPUT->header();
-echo $OUTPUT->render_from_template('local_reblibrary/navigation', local_reblibrary_get_navigation('index'));
+echo $OUTPUT->render_from_template('local_reblibrary/header', $headercontext);
 echo $OUTPUT->render_from_template('local_reblibrary/library_home', $templatecontext);
 echo $OUTPUT->footer();
--- lang/en/local_reblibrary.php
+++ lang/en/local_reblibrary.php
@@ -26,19 +26,42 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'REB Library';
 
-// Navigation strings.
-$string['nav_home'] = 'Home';
-$string['nav_browse'] = 'Browse';
-$string['nav_search'] = 'Search';
-$string['nav_collection'] = 'My Collection';
+// Header strings.
+$string['header_logo'] = 'library.';
+$string['header_search_placeholder'] = 'Search...';
+$string['header_subscribe'] = 'Subscribe';
+
+// Sidebar navigation strings.
+$string['sidebar_browse'] = 'BROWSE';
+$string['sidebar_your_books'] = 'YOUR BOOKS';
+$string['sidebar_shelves'] = 'SHELVES';
+
+// Browse section.
+$string['nav_topbooks'] = 'Top Books';
+$string['nav_discover'] = 'Discover';
+$string['nav_categories'] = 'Categories';
+
+// Your Books section.
+$string['nav_reading'] = 'Reading';
+$string['nav_favorite'] = 'Favorite Reads';
+$string['nav_history'] = 'History';
+
+// Shelves section.
+$string['nav_your_shelves'] = 'Your Shelves';
+$string['btn_create_shelf'] = 'Create a Shelf';
+
+// Main content sections.
+$string['section_recently_added'] = 'Recently Added';
+$string['section_recommended'] = 'Recommended For You';
+
+// Friends sidebar.
+$string['friends_heading'] = 'YOUR FRIENDS';
 
 // Library home page strings.
 $string['librarypage_title'] = 'REB Library';
 $string['librarypage_heading'] = 'REB Library';
 $string['librarypage_welcome'] = 'Welcome to REB Library';
 $string['librarypage_description'] = 'Browse and explore educational resources tailored for your learning journey.';
-$string['librarypage_placeholder_title'] = 'Coming Soon';
-$string['librarypage_placeholder_text'] = 'The REB Library is currently under development. New features and resources will be available soon.';
 
 // Browse page strings.
 $string['browsepage_title'] = 'Browse Resources';
